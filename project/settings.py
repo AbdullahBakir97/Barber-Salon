ca05a4f96be6bb3575262fa0fd73f917608c4ce4
Abdullah Bakir <abdullah.bakir.204@gmail.com>
2024-03-03 16:00:22 +0100

    add django-admin-simpleui

--- project/settings.py
+++ project/settings.py
@@ -34,7 +34,8 @@ ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
     'accounts',
-    'bootstrap_admin',
+    # 'bootstrap_admin',
+    'simpleui',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
