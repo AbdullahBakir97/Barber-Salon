d6726852db0bd1904dbebc09752fe2002df93d7f
Abdullah Bakir <abdullah.bakir.204@gmail.com>
2024-04-02 03:34:05 +0200

    Update settings.py

--- project/settings.py
+++ project/settings.py
@@ -16,7 +16,7 @@ import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-DEBUG= True
+DEBUG= False
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
