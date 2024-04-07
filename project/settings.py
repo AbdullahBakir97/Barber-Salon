cbb04c70667d3a85d42afba150a7a11ad9fe4996
Abdullah Bakir <abdullah.bakir.204@gmail.com>
2024-04-07 07:09:31 +0200

    ediz

--- project/settings.py
+++ project/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = DEBUG= os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 
 
-ALLOWED_HOSTS = ["www.kharsa-style.de"]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
