090a40ed3fd12e3721fb4524e9e764e9c1d8747f
Abdullah Bakir <abdullah.bakir.204@gmail.com>
2024-04-08 00:27:27 +0200

    settings.py aktualisieren

--- project/settings.py
+++ project/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 handler404 = 'settings.views.handl404'
 handler500 = 'settings.views.handl500'
