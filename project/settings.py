f782370580f25286a0e46edc1c6142c7286b1aea
Abdullah Bakir <abdullah.bakir.204@gmail.com>
2024-04-07 07:00:16 +0200

    fix host

--- project/settings.py
+++ project/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = DEBUG= os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 
 
-ALLOWED_HOSTS = ["127.0.0.1"]
+ALLOWED_HOSTS = ["www.kharsa-style.de"]
 
 
 # Application definition
