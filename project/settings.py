fd94ba912eaca9fe92582b69074028afe991b955
Abdullah Bakir <abdullah.bakir.204@gmail.com>
2024-04-13 22:08:43 +0200

    settings.py aktualisieren

--- project/settings.py
+++ project/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 allowed_hosts_string = os.environ.get('ALLOWED_HOSTS')
 
