016b0cd4ab3532b0443e67c2b7636351e4b0466b
Abdullah Bakir <abdullah.bakir.204@gmail.com>
2024-04-14 00:33:21 +0200

    settings.py aktualisieren

--- project/settings.py
+++ project/settings.py
@@ -217,14 +217,18 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_URL = '/media/'
+
 if DEBUG:
-        STATICFILES_DIRS = [ os.path.join(BASE_DIR, 'static')]
+
+    STATICFILES_DIRS = [os.path.join(BASE_DIR, "staticfiles")]
+
 else:
-        STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-MEDIA_URL =  '/media/'
-MEDIA_ROOT =  os.path.join(BASE_DIR, "media")
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
