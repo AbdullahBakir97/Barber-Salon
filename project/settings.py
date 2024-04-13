4d4f04dd3d28506a6dec1c042c3ca1fb671d290e
Abdullah Bakir <abdullah.bakir.204@gmail.com>
2024-04-13 18:56:01 +0200

    edit env

--- project/settings.py
+++ project/settings.py
@@ -29,9 +29,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS')
 
 
 # Application definition
