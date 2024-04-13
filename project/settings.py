b401c31c8fd301729df3b0f0ae82cbc16e41a636
Abdullah Bakir <abdullah.bakir.204@gmail.com>
2024-04-13 19:45:29 +0200

    .

--- project/settings.py
+++ project/settings.py
@@ -19,7 +19,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
