39a9c054fbe89307fdfbfcb8aeb2f63f8443a50f
Abdullah Bakir <abdullah.bakir.204@gmail.com>
2024-04-13 18:47:31 +0200

    edit staticfiles

--- project/settings.py
+++ project/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
