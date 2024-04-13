7ba5bf1214141b2a3bb7620af129f21706d1801b
Abdullah Bakir <abdullah.bakir.204@gmail.com>
2024-04-13 03:41:08 +0200

    edit static

--- project/settings.py
+++ project/settings.py
@@ -222,7 +222,7 @@ WHITENOISE_STATIC_PREFIX = '/static/'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL =  '/media/'
-MEDIA_ROOT =  BASE_DIR / "media"
+MEDIA_ROOT =  os.path.join(BASE_DIR, "media")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
