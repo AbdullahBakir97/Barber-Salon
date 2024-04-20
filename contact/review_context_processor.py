ff6350d6a3d3d87698a6ba27091d881afc91d348
Abdullah Bakir <abdullah.bakir.204@gmail.com>
2024-04-21 01:11:20 +0200

    fixes

--- contact/review_context_processor.py
+++ contact/review_context_processor.py
@@ -1,14 +1,14 @@
-from .models import Review
+from .models import Review, Barber
 from django.core.paginator import Paginator
 
 def get_review_data(request):
     all_reviews = Review.objects.order_by('-id')
-
+    barber_list = Barber.objects.all()
     # Pagination
     paginator = Paginator(all_reviews, 5)
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
 
     return {
-        'review_data': page_obj,
+        'review_data': page_obj, 'barber_list':barber_list
     }
